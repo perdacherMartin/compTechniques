640b26c314342a1cda548fdb72bae2e9ed3ef46c
minkleo <liumingming@hotmail.com>
2013-01-10 10:16:31 +0100

    update

--- GenTsp/test/at/univie/ct/ga/data/GenerationTest.java
+++ GenTsp/test/at/univie/ct/ga/data/GenerationTest.java
@@ -2,6 +2,7 @@ package at.univie.ct.ga.data;
 
 import java.io.IOException;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 import at.univie.ct.ga.GAMain;
@@ -10,7 +11,7 @@ import at.univie.ct.ga.InitData;
 
 public class GenerationTest {
 	
-	@Test
+	@Ignore
 	public void testForOrderOX() throws IOException{
 		GAMain main = new GAMain();
 		InitData.setCityMapAndNumberOfCity(main.filePath);
@@ -32,6 +33,34 @@ public class GenerationTest {
 		System.out.print("OX: ");
 		Individual.distance = InitData.getCityDistance(InitData.cityMap);
 		Individual temp = gen.orderOX(f1, f2);
+		for(City c: temp.myPath){
+			System.out.print(c.getNumber() + " ");
+		}
+		
+	}
+	@Test
+	public void testForPMX() throws IOException{
+		
+		GAMain main = new GAMain();
+		InitData.setCityMapAndNumberOfCity(main.filePath);
+		Generation gen = new Generation(main.populationSize);
+		Individual.distance = InitData.getCityDistance(InitData.cityMap);
+		Individual f1 = GAMain.randomlyGenerateIndividuals();
+		
+		Individual f2 = GAMain.randomlyGenerateIndividuals();
+		System.out.print("Path1: ");
+		for(City c: f1.myPath){
+			System.out.print(c.getNumber() + " ");
+		}
+		System.out.print("\n");
+		System.out.print("Path2: ");
+		for(City c: f2.myPath){
+			System.out.print(c.getNumber() + " ");
+		}
+		System.out.print("\n");
+		System.out.print("PMX: ");
+		Individual.distance = InitData.getCityDistance(InitData.cityMap);
+		Individual temp = gen.pmx(f1, f2);
 		for(City c: temp.myPath){
 			System.out.print(c.getNumber() + " ");
 		}
