f4b9de3ca7ec2324dd3e16a57cbaa63a8140ed6b
minkleo <liumingming@hotmail.com>
2013-01-10 10:16:08 +0100

    update

--- GenTsp/src/at/univie/ct/ga/data/Generation.java
+++ GenTsp/src/at/univie/ct/ga/data/Generation.java
@@ -16,7 +16,8 @@ public class Generation {
 	/**
 	 * Constructor
 	 * 
-	 * @param m
+	 * @param populationSize
+	 *         size of population
 	 *            
 	 */
 	
@@ -71,36 +72,6 @@ public class Generation {
 		return this;
 	}
  
-	public Individual getminPath() {
-		if (this.minPath != null) {
-			return this.minPath;
-		}
- 
-		findMin();
-		return this.minPath;
-	}
-	
-	public double getMinCost() {
-		if (this.minCost - Double.MIN_VALUE > 0) {
-			return this.minCost;
-		}
- 
-		findMin();
-		this.minCost = this.minPath.fitness;
- 
-		return this.minCost;
-	}
- 
-	
-	public String toString() {
-		StringBuilder sb = new StringBuilder();
-    	for(City c: this.minPath.myPath){
-    		sb.append(c.getNumber()+ " ");
-    	}
-		return  " minPath is "
-				+ sb.toString() + " and the path costs " + getMinCost()
-				+ "\n";
-	}
  
 	/**
 	 * Choose the OderOX or pmx.
@@ -131,6 +102,7 @@ public class Generation {
 	public Individual orderOX(Individual f1, Individual f2) {
 		int length = InitData.numberOfCity - 1;
 		Random rand = new Random();
+		// radom nummber from 0 to length
     	int r1 = rand.nextInt(length - 0 + 1) + 0; 
     	int r2 = rand.nextInt(length - 0 + 1) + 0; 
     	while(r1==r2){
@@ -208,7 +180,7 @@ public class Generation {
 	
 	/**
 	 * PMX
-	 * 
+	 * Wen can not use PMX for TSP Problem.
 	 * @param f1
 	 * @param f2
 	 *            two Individuals
@@ -218,6 +190,87 @@ public class Generation {
 	public Individual pmx(Individual f1, Individual f2){
 		ArrayList<City> sonPath = new ArrayList<City>();
 		//TODO
+		int length = InitData.numberOfCity - 1;
+		Random rand = new Random();
+		// radom nummber from 1 to length-1
+    	int r1 = rand.nextInt(length-1 - 1 + 1) + 1; 
+    	int r2 = rand.nextInt(length-1 - 1 + 1) + 1; 
+    	while(r1==r2){
+    		r1 = rand.nextInt(length-1 - 1 + 1) + 1; 
+    		r2 = rand.nextInt(length-1 - 1 + 1) + 1;
+    	}
+    	//----------only for test-------------------
+    	
+    	System.out.println("R1: " + r1 + " R2: " + r2);
+    	//------------------------------------------
+    	for(int xx=0;xx<=length;xx++){
+    		City c = new City();
+    		c.setNumber(0);
+    		sonPath.add(c);
+    	}
+    	int index = -1;
+    	if(r1>r2){
+    		for(int i=r2;i<=r1;i++){
+    			sonPath.set(i, f1.myPath.get(i));
+    		}
+    		for(int j=0;j<=length;j++){
+    			
+    			for(int k=r2;k<=r1;k++){
+    				if(f2.myPath.get(j).getNumber()== f1.myPath.get(k).getNumber()){
+    					index = f1.myPath.indexOf(f1.myPath.get(k));
+    				}
+    			}
+    			
+    			if(index > -1){
+    				if(j>=r2 && j<=r1){
+    					
+    				} else {
+    					sonPath.set(j, f2.myPath.get(index));
+    				}
+    				
+    			} else {
+    				if(j>=r2 && j<=r1){
+    					
+    				} else {
+    					sonPath.set(j, f2.myPath.get(j));
+    				}
+    				
+    			}
+    			index = -1;
+    		}
+    		
+    	} else {
+    		for(int i=r1;i<=r2;i++){
+    			sonPath.set(i, f1.myPath.get(i));
+    		}
+    		for(int j=0;j<=length;j++){
+    			
+    			for(int k=r1;k<=r2;k++){
+    				if(f2.myPath.get(j).getNumber()== f1.myPath.get(k).getNumber()){
+    					index = f1.myPath.indexOf(f1.myPath.get(k));
+    				}
+    			}
+    			
+    			if(index > -1){
+    				if(j>=r1 && j<=r2){
+    					
+    				} else {
+    					sonPath.set(j, f2.myPath.get(index));
+    				}
+    				
+    			} else {
+    				if(j>=r1 && j<=r2){
+    					
+    				} else {
+    					sonPath.set(j, f2.myPath.get(j));
+    				}
+    				
+    			}
+    			index = -1;
+    		}
+    		
+    	}
+    	
 		return new Individual(sonPath);
 	}
  
@@ -241,5 +294,35 @@ public class Generation {
  
 		this.minPath = min;
 	}
+
+	public Individual getminPath() {
+		if (this.minPath != null) {
+			return this.minPath;
+		}
  
+		findMin();
+		return this.minPath;
+	}
+	
+	public double getMinCost() {
+		if (this.minCost - Double.MIN_VALUE > 0) {
+			return this.minCost;
+		}
+ 
+		findMin();
+		this.minCost = this.minPath.fitness;
+ 
+		return this.minCost;
+	}
+ 
+	
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+    	for(City c: this.minPath.myPath){
+    		sb.append(c.getNumber()+ " ");
+    	}
+		return  " minPath is "
+				+ sb.toString() + " and the path costs " + getMinCost()
+				+ "\n";
+	}
 }
